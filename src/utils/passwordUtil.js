1e37eb27f7d1b8f646b17cfd60b0d3c9e0550593
theDuyet-Shine <nguyenduyethp.working@gmail.com>
2024-05-22 09:25:18 +0700

    small update

--- src/utils/passwordUtil.js
+++ src/utils/passwordUtil.js
@@ -1,14 +1,9 @@
-import bcrypt from "bcrypt";
-
-const saltRounds = 10;
+import bcrypt from "bcryptjs";
 
 const hashPassword = async (password) => {
-  try {
-    const salt = await bcrypt.genSalt(saltRounds);
-    return await bcrypt.hash(password, salt);
-  } catch (error) {
-    throw error;
-  }
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+  return hashedPassword;
 };
 
 const comparePassword = async (password, hash) => {
