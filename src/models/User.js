0072421888d96d9416f73a5614100201333c2544
theDuyet-Shine <nguyenduyethp.working@gmail.com>
2024-06-10 16:00:40 +0700

    update api

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,35 @@
 import mongoose from "mongoose";
 
+let profile_image_name_list = [
+  "Lola",
+  "Snickers",
+  "Annie",
+  "Ginger",
+  "Mitten",
+  "Milo",
+  "Lucy",
+  "Simon",
+  "Loky",
+  "Casper",
+  "Spooky",
+];
+
+const getRandomName = () => {
+  const randomIndex = Math.floor(
+    Math.random() * profile_image_name_list.length
+  );
+  return profile_image_name_list[randomIndex];
+};
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   fullname: { type: String, required: true },
   profile_image: {
     type: String,
-    default:
-      "https://th.bing.com/th/id/OIP.ZXSgz-LNexQRb5OgjOrr6wHaFv?rs=1&pid=ImgDetMain",
+    default: () => {
+      return `https://api.dicebear.com/8.x/avataaars/svg?seed=${getRandomName()}`;
+    },
   },
   address: { type: String, default: null },
   phone_number: { type: String, default: null },
