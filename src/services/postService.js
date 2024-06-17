d85d8391e5afd410c3a6ac0e3776077e8dafa133
theDuyet-Shine <nguyenduyethp.working@gmail.com>
2024-06-18 06:32:47 +0700

    update post service

--- src/services/postService.js
+++ src/services/postService.js
@@ -5,13 +5,25 @@ import {
   getPostById,
   updatePostById,
 } from "../repositories/PostRepo.js";
+import { updateUserById } from "../repositories/UserRepo.js";
 
 export const createPostService = async (postData) => {
-  return await createPost(postData);
+  try {
+    const newPost = await createPost(postData);
+
+    await updateUserById(postData.author, { $inc: { total_post: 1 } });
+
+    return newPost;
+  } catch (error) {
+    console.error("Error in createPostService:", error);
+    throw error;
+  }
 };
 
 export const getPostByIdService = async (id) => {
-  return await getPostById(id);
+  const post = await getPostById(id);
+  await updatePostById(post._id, { $inc: { total_views: 1 } });
+  return post;
 };
 
 export const updatePostService = async (id, updateData) => {
